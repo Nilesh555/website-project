931a93f011c2b6e0ff6b8f80e6758447657ace82
Chudasama Nilesh <chudasamadhruva222@gmail.com>
2025-08-24 20:14:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let direction = { x: 0, y: 0 };
+let foodSound = new Audio('food.mp3');
+let gameOverSound = new Audio('gameover.mp3');
+let moveSound = new Audio('move.mp3');
+let musicSound = new Audio('music.mp3');
+let speed = 0;
+let lastPaintTime = 0;
+let snakeArr = [
+    { x: 13, y: 15 }
+]
+
+
+
+
+
+function main(ctime) {
+
+    window.requestAnimationFrame(main);
+    console.log(ctime);
+    if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
+        return;
+    }
+    lastPaintTime = ctime;
+    main();
+
+}
+
+function gameEngine() {
+
+    board.innerHTML = "";
+    snakeArr.forEach((e, index) => {
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        snakeElement.classList.add('food');
+        board.appendChild(snakeElement);
+
+
+
+    })
+
+
+}
+
+
+
+window.requestAnimationFrame(main);
+
